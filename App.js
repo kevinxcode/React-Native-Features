43898a098a92bb872ff997658d19271ecdec1db2
kevinvscode1 <kevinxcode924@gmail.com>
2022-10-15 13:48:44 +0700

    flexbox-box

--- App.js
+++ App.js
@@ -3,13 +3,35 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{
-      backgroundColor: 'red',
-      width: 100,
-      height: 100,
-      justifyContent: 'center',
-      alignItems: 'center'
-    }}></View>
+    <View style={{padding: 50}}>
+      <View style={{
+        backgroundColor: 'red',
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>box 1</Text>
+      </View>
+      <View style={{
+        backgroundColor: 'blue',
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>Box 2</Text>
+      </View>
+      <View style={{
+        backgroundColor: 'green',
+        width: '100%',
+        height: 100,
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}>
+        <Text>Box 3</Text>
+      </View>
+    </View>
   );
 }
 
