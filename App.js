3f27a704e5edaa3cc5ff7f72eddd253aa277f81f
kevinvscode1 <kevinxcode924@gmail.com>
2022-10-15 12:06:58 +0700

    style view

--- App.js
+++ App.js
@@ -3,37 +3,18 @@ import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
-      <View style={{marginBottom: 10}}>
-        <Text >another text</Text>
+    <View>
+      <View>
+        <TextInput placeholder=' your course goal!'/>
+        <Button title='Add Goal' />
       </View>
-    {/* inline css  */}
-      <Text style={{margin: 16, borderWidth: 2, borderColor: 'red', padding: 16}}>INLINE STYLE</Text> 
-      {/* css in constan */}
-      <Text style={styles.dummyText}>Login</Text>
-    
-      <Button title='Tap Me' />
-
+      <View></View>
+      <View></View>
     </View>
   );
 }
 
-const style2 = StyleSheet.create({
-
-})
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText:{
-    margin: 16,
-    borderWidth: 2,
-    borderColor: 'blue',
-    padding: 16
-  }
+ 
 });
