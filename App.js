088f849e48b0eb6c7321a6be949ec16980fe7758
kevinvscode1 <kevinxcode924@gmail.com>
2022-10-15 11:32:46 +0700

    First

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Login</Text>
+      <TextInput>aa</TextInput>
+      <Button
+  onPress={() => {
+    alert('You tapped the button!');
+  }}
+  title="Press Me"
+/>
       <StatusBar style="auto" />
     </View>
   );
