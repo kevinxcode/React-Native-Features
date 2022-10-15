ef6f020ac5a63f88faf674630b3ac53304a0e305
kevinvscode1 <kevinxcode924@gmail.com>
2022-10-15 14:45:07 +0700

    flexbox

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder=' your course goal!'/>
         <Button title='Add Goal' />
       </View>
-      <View style={styles.appContent}>
+      <View style={styles.goalsContainer}>
         <Text >List of goals...</Text>
       </View>
      
@@ -19,20 +19,28 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer:{
+    flex: 1,
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput:{
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 6
   },
-  appContent:{
-    paddingTop: 20,
-  }
+  goalsContainer:{
+    flex: 5,
+  },
+  
 });
