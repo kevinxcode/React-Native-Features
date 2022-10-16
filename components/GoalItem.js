5e441bc6be612cb4ece456a08de3b8965632af70
kevinvscode1 <kevinxcode924@gmail.com>
2022-10-16 16:02:53 +0700

    press effect

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,15 @@
 import {StyleSheet, Text, View, Pressable} from 'react-native'
 function GoalItem(props){
     return (
-        <Pressable onPress={props.onDeleteItem.bind(this,props.id)}>
         <View style={styles.goalItem} >
+        <Pressable 
+        android_ripple={{color: '#dddddd'}} // andoid riple
+        onPress={props.onDeleteItem.bind(this,props.id)}
+        style={({pressed}) => pressed && styles.pressItem}// press style  ios / android
+        >
             <Text style={styles.goalText}>{props.text}</Text>
-          </View>
         </Pressable>
+        </View>
     );
 };
 export default GoalItem;
@@ -14,11 +18,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
     goalItem: {
         margin: 8,
-        padding: 8,
         borderRadius: 6,
         backgroundColor: '#5e0acc',
       },
+      pressItem:{
+        backgroundColor: '#dddddd',
+      },
       goalText: {
+        padding: 8,
         color: 'white',
       }
 });
